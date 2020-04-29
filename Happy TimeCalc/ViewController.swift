7a7dede7cb6d120771061da61fcc10449ebfdb41
David Pai <dbai2.0@gmail.com>
2020-04-29 11:22:28 +0800

    Initial Commit

---
+++ Happy TimeCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Happy TimeCalc
+//
+//  Created by David Pai on 2020/4/29.
+//  Copyright © 2020 Pai Bros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
