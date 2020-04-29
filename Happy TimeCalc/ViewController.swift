55f6edb5920bb71df28caa8bc8852ec2dccf9e4f
Luke Pai <lukepai2.0@icloud.com>
2020-04-30 01:03:48 +0800

    done

--- Happy TimeCalc/ViewController.swift
+++ Happy TimeCalc/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var dummyTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var dummyBottomContraint: NSLayoutConstraint!
     
@@ -22,23 +23,47 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
 //        dummyBottomContraint.constant = 400
         
+        print("UIView's frame.self: \(self.view.frame.self)")
+        print("UIView's safeAreaInsets: \(self.view.safeAreaInsets)")
         print("UIScreen.main.bounds.maxY: \(UIScreen.main.bounds.maxY)")
-//        print("ScrollView's bottom: \(scrollView.frame.maxY)")
-        print("scrollView.frame.height: \(scrollView.frame.height)")
-        print("self.view.frame.height: \(self.view.frame.height)")
-        print("Safe Area height: \(getSafeAreaHeight())")
-        print("self.view.safeAreaLayoutGuide.layoutFrame.height: \(self.view.safeAreaLayoutGuide.layoutFrame.height)")
+        print("scrollView.frame.self: \(scrollView.frame.self)")
+        print("contentView.frame.self: \(contentView.frame.self)")
+//        print("scrollView.frame.height: \(scrollView.frame.height)")
+//        print("self.view.frame.height: \(self.view.frame.height)")
+//        print("Safe Area height: \(getSafeAreaHeight())")
+//        print("self.view.safeAreaLayoutGuide.layoutFrame.height: \(self.view.safeAreaLayoutGuide.layoutFrame.height)")
+        print("======")
+                        
+        label.center.y = 718//UIScreen.main.bounds.maxY
         
-        label.center.y = 0//UIScreen.main.bounds.maxY
+        print("scrollView.frame.self: \(scrollView.frame.self)")
+        print("scrollView.frame.minY: \(scrollView.frame.minY)")
+        print("scrollView.frame.maxY: \(scrollView.frame.maxY)")
+        print("contentView.frame.self: \(contentView.frame.self)")
+        print("label.frame.maxY: \(label.frame.maxY)")
+        print("label.frame.midY: \(label.frame.midY)")
+        print("label.frame.origin.y: \(label.frame.origin.y)")
         print("label.bounds.midY: \(label.bounds.midY)")
+        print("label.bounds.origin.y: \(label.bounds.origin.y)")
+        print("label.frame.self: \(label.frame.self)")
         
         let realBottomOfDummy = scrollView.frame.height - dummyTopConstraint.constant
-        print("dummyBottomContraint.constant: \(dummyBottomContraint.constant)")
+        print("realBottomOfDummy: \(realBottomOfDummy)")
+        print("Before modified dummyBottomContraint.constant: \(dummyBottomContraint.constant)")
         
-        if dummyBottomContraint.constant >= realBottomOfDummy {
-            dummyBottomContraint.constant = realBottomOfDummy
-            print("Modified dummyBottomContraint.constant: \(dummyBottomContraint.constant)")
+        if label.frame.maxY > scrollView.frame.height {
+            print("Longer!")
+            dummyBottomContraint.constant = label.frame.maxY - dummyTopConstraint.constant
+            print("After modified dummyBottomContraint.constant: \(dummyBottomContraint.constant)")
+            
+            viewDidLayoutSubviews()
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("~~~~~~\ncontentView.frame.self: \(contentView.frame.self)")
     }
 
     func getSafeAreaHeight() -> CGFloat {
