18767d26ad72048fa158940574db044ce0afb251
David Pai <dbai2.0@gmail.com>
2020-04-29 18:58:58 +0800

    first

---
+++ Happy TimeCalc/TestViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TestViewController.swift
+//  Happy TimeCalc
+//
+//  Created by David Pai on 2020/4/29.
+//  Copyright © 2020 Pai Bros. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(false)
+        
+        label.frame.origin.y = 20
+        print("label.bounds.maxY: \(label.bounds.maxY)")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Happy TimeCalc/ViewController.swift
+++ Happy TimeCalc/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var dummyTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var dummyBottomContraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+     
+    override func viewDidAppear(_ animated: Bool) {
+//        dummyBottomContraint.constant = 400
+        
+        print("UIScreen.main.bounds.maxY: \(UIScreen.main.bounds.maxY)")
+//        print("ScrollView's bottom: \(scrollView.frame.maxY)")
+        print("scrollView.frame.height: \(scrollView.frame.height)")
+        print("self.view.frame.height: \(self.view.frame.height)")
+        print("Safe Area height: \(getSafeAreaHeight())")
+        print("self.view.safeAreaLayoutGuide.layoutFrame.height: \(self.view.safeAreaLayoutGuide.layoutFrame.height)")
+        
+        label.center.y = 0//UIScreen.main.bounds.maxY
+        print("label.bounds.midY: \(label.bounds.midY)")
+        
+        let realBottomOfDummy = scrollView.frame.height - dummyTopConstraint.constant
+        print("dummyBottomContraint.constant: \(dummyBottomContraint.constant)")
+        
+        if dummyBottomContraint.constant >= realBottomOfDummy {
+            dummyBottomContraint.constant = realBottomOfDummy
+            print("Modified dummyBottomContraint.constant: \(dummyBottomContraint.constant)")
+        }
     }
 
+    func getSafeAreaHeight() -> CGFloat {
+        let verticalSafeAreaInset: CGFloat
+        if #available(iOS 11.0, *) {
+            verticalSafeAreaInset = self.view.safeAreaInsets.bottom + self.view.safeAreaInsets.top
+            print("self.view.safeAreaInsets.bottom: \(self.view.safeAreaInsets.bottom),  self.view.safeAreaInsets.top: \(self.view.safeAreaInsets.top)")
+        } else {
+            verticalSafeAreaInset = 0.0
+            print("2")
+        }
+        let safeAreaHeight = self.view.frame.height - verticalSafeAreaInset
+        
+//        let safeAreaHeight = self.view.frame.height - self.topLayoutGuide.length - self.bottomLayoutGuide.length
+        
+//        let safeAreaHeight = self.view.safeAreaLayoutGuide.
 
+        return safeAreaHeight
+    }
+    
 }
 
